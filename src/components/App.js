8ef72ec9be17eb1e4c42b71b90215a8890ad44c3
Seano <sean01234@yahoo.com>
2023-06-19 00:04:03 -0400

    add cool dogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React,{useState,useEffect} from 'react'
+
+const API_URL="https://dog.ceo/api/breeds/image/random";
+
+function App() {
+  const [image,setImage]=useState(null)
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setImage(data.message))
+  },[])
+  if(!image) {
+    return <p className="cooldog">Loading...</p>
+  }
+
+  return (
+    <div>
+      <p className="cooldog">Here's a cool pup for you:</p>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+
+export default App
